a9e6e9e364168a5954f5bc806f90350ce9e2f4d2
vb64 <vit.sar68@gmail.com>
2025-03-18 22:51:47 +0400

    step

--- compare/to_text.py
+++ compare/to_text.py
@@ -1,5 +1,50 @@
+def diarization(in_file, out_file):
+    print(in_file, "->", out_file)
+
+
+def content(in_file, out_file):
+    print(in_file, "->", out_file)
+
+
+def is_complete(sentence):
+    return sentence[-1] in ".!?:"
+
+
+def write_sentence(out, text):
+    out.write(text)
+    out.write('\n')
+
+
+def whisper(in_file, out_file):
+    print(in_file, "->", out_file)
+    lines = open(in_file, "rt", encoding="utf-8").readlines()
+    start = 0
+    text = []
+    eof = False
+    while not eof:
+        chunk = lines[start:start + 4]
+        start += 4
+        if len(chunk) < 4:
+            eof = True
+        else:
+            text.append(chunk[2].strip('\n'))
+
+    out = open(out_file, "wt", encoding="utf-8")
+    group = []
+    for sentence in text:
+        group.append(sentence)
+        if is_complete(sentence):
+            write_sentence(out, ' '.join(group))
+            group = []
+
+    out.close()
+
+
 def main():
-    print("Handle whisper.srt")
+    whisper("whisper.srt", "whisper.txt")
+    diarization("diarization.srt", "diarization.txt")
+    content("content.md", "content.txt")
+
 
 if __name__ == '__main__':
     main()
