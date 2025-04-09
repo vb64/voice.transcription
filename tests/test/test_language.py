cd16e626e68570c020cc350ceba95e3d840fa237
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-04-09 15:46:44 +0400

    language.py

---
+++ tests/test/test_language.py
@@ -0,0 +1,26 @@
+"""Module language.py tests.
+
+make test T=test_language.py
+"""
+import pytest
+
+from . import TestBase
+
+
+class TestLanguage(TestBase):
+    """Module language."""
+
+    def test_process_language_arg(self):
+        """Check process_language_arg function."""
+        from voice_transcription.language import process_language_arg
+        from voice_transcription import Model
+
+        assert process_language_arg('ru', Model.Large) == 'ru'
+
+        with pytest.raises(ValueError) as err:
+            process_language_arg('ru', 'large.en')
+        assert 'English-only model' in str(err.value)
+
+        with pytest.raises(ValueError) as err:
+            process_language_arg('xxx', 'large.en')
+        assert 'Unsupported language' in str(err.value)
--- voice_transcription/transcript.py
+++ voice_transcription/transcript.py
@@ -3,16 +3,25 @@ import sys
 import time
 
 from .cli_options import PARSER, VERSION, COPYRIGHTS
+from .language import process_language_arg
+from . import Model
+
+LANGUAGE = 'ru'
 
 
 def main(options):
     """Entry point."""
-    print("Voice to text tool v.{}. {}".format(VERSION, COPYRIGHTS))
-    print("File: '{}' speakers: {}".format(
-      options.input_file,
-      options.num_speakers if options.num_speakers > 0 else 'auto'
-    ))
     start_time = time.time()
+    print("Voice to text tool v.{}. {}".format(VERSION, COPYRIGHTS))
+
+    lang = process_language_arg(LANGUAGE, Model.Large)
+
+    print("File: '{}' speakers: {} language: {}".format(
+      options.input_file,
+      options.num_speakers if options.num_speakers > 0 else 'auto',
+      lang
+    ))
+
     print("\nTotal: {} sec".format(int(time.time() - start_time)))
 
     return 0
