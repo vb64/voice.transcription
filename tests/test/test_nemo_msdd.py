53c5ca7ae2cbffe2c0f661eaf1abc99fccfdf7be
vb64 <vit.sar68@gmail.com>
2025-04-10 19:24:02 +0400

    step

--- tests/test/test_nemo_msdd.py
+++ tests/test/test_nemo_msdd.py
@@ -2,7 +2,7 @@
 
 make test T=test_nemo_msdd.py
 """
-# import pytest
+import pytest
 
 from . import TestBase
 
@@ -13,10 +13,31 @@ class TestNemo(TestBase):
     def setUp(self):
         """Set defaults."""
         super().setUp()
+
+        from voice_transcription import nemo_msdd
+
         self.wav = self.fixture('mono.wav')
+        self.model_config_path = nemo_msdd.MODEL_CONFIG_PATH
+        nemo_msdd.MODEL_CONFIG_PATH = self.fixture('nemo_msdd_configs', 'diar_infer_telephonic.yaml')
+
+    def tearDown(self):
+        """Restore defaults."""
+        from voice_transcription import nemo_msdd
+
+        nemo_msdd.MODEL_CONFIG_PATH = self.model_config_path
+        super().tearDown()
 
     def test_create_config(self):
         """Check create_config function."""
-        from voice_transcription.nemo_msdd import create_config
+        from voice_transcription import nemo_msdd
 
-        assert create_config(self.wav, 'build', num_speakers=0)
+        assert nemo_msdd.create_config(self.wav, 'build', num_speakers=0)
+
+    @pytest.mark.longrunning
+    def test_diarize(self):
+        """Check diarize function."""
+        from voice_transcription.nemo_msdd import diarize
+        from voice_transcription import Device
+
+        call_log = []
+        assert diarize(call_log, self.wav, Device.Cpu, 2, 'build') == 0
