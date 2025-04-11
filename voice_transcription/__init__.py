578834cafb883b6531b391fe2642124b41c99b89
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-04-11 15:59:41 +0400

    debug.print

--- voice_transcription/__init__.py
+++ voice_transcription/__init__.py
@@ -31,8 +31,13 @@ def add_log(call_log, name, start_time):
     """Add record to call log."""
     now = time.time()
     seconds = None
+
     if start_time is not None:
         seconds = int(now - start_time)
+        print("{}: {} sec".format(name, seconds))
+    else:
+        print("# Call: {}".format(name))
+
     call_log.append((name, seconds))
 
     return now
