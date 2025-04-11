452b6acf5659051dc13050e74d516ae2a0b85bdf
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-04-11 11:57:06 +0400

    step

---
+++ voice_transcription/srt.py
@@ -0,0 +1,43 @@
+"""Srt file."""
+from . import add_log
+
+
+def format_timestamp(
+    milliseconds: float,
+    always_include_hours: bool = False,
+    decimal_marker: str = "."
+):
+    """Convert timestamp to string."""
+    assert milliseconds >= 0, "non-negative timestamp expected"
+
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
+
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
+
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+
+    hours_marker = f"{hours:02d}:" if always_include_hours or hours > 0 else ""
+    return (
+        f"{hours_marker}{minutes:02d}:{seconds:02d}{decimal_marker}{milliseconds:03d}"
+    )
+
+
+def write_srt(call_log, transcript, file_name):
+    """Write a transcript to a file in SRT format."""
+    start_time = add_log(call_log, "write_srt", None)
+
+    for i, segment in enumerate(transcript, start=1):
+        # write srt lines
+        print(
+            f"{i}\n"
+            f"{format_timestamp(segment['start_time'], always_include_hours=True, decimal_marker=',')} --> "
+            f"{format_timestamp(segment['end_time'], always_include_hours=True, decimal_marker=',')}\n"
+            f"{segment['speaker']}: {segment['text'].strip().replace('-->', '->')}\n",
+            file=file,
+            flush=True,
+        )
+
+    add_log(call_log, "done write_srt", start_time)
