fa987f24f9e92b5478a50f0f57f2c2f96d2f7f0d
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-04-11 12:36:34 +0400

    step

--- voice_transcription/srt.py
+++ voice_transcription/srt.py
@@ -28,6 +28,7 @@ def format_timestamp(
 def write_srt(call_log, transcript, file_name):
     """Write a transcript to a file in SRT format."""
     start_time = add_log(call_log, "write_srt", None)
+    srt = open(file_name, "w", encoding="utf-8")
 
     for i, segment in enumerate(transcript, start=1):
         # write srt lines
@@ -36,8 +37,9 @@ def write_srt(call_log, transcript, file_name):
             f"{format_timestamp(segment['start_time'], always_include_hours=True, decimal_marker=',')} --> "
             f"{format_timestamp(segment['end_time'], always_include_hours=True, decimal_marker=',')}\n"
             f"{segment['speaker']}: {segment['text'].strip().replace('-->', '->')}\n",
-            file=file,
+            file=srt,
             flush=True,
         )
 
+    srt.close()
     add_log(call_log, "done write_srt", start_time)
