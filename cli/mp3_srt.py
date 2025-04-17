3fb5125d289a677874aef6f6c90986604664d5aa
vb64 <vit.sar68@gmail.com>
2025-04-17 23:57:02 +0400

    step

--- cli/mp3_srt.py
+++ cli/mp3_srt.py
@@ -2,12 +2,28 @@
 import argparse
 import os
 import sys
+import time
+
+import torch
+import faster_whisper
+from ctc_forced_aligner import (
+  generate_emissions,
+  get_alignments,
+  get_spans,
+  load_alignment_model,
+  postprocess_results,
+  preprocess_text,
+)
 
 sys.path.insert(1, '.')
-from voice_transcription import list_mp3
+from voice_transcription import list_mp3, Model, Device, MTYPES, TTYPES
+from voice_transcription.speaker_mapping import map_speakers
+from voice_transcription.srt import write_srt
+from voice_transcription.transcript import transcribe
 
 VERSION = '1.0'
 COPYRIGHTS = 'Copyrights by Vitaly Bogomolov 2025'
+LANGUAGE = 'ru'
 PARSER = argparse.ArgumentParser(description='Whisper transcribe tool.')
 
 PARSER.add_argument(
@@ -31,8 +47,30 @@ PARSER.add_argument(
 def main(options):
     print("Whisper transcribe tool v.{}. {}".format(VERSION, COPYRIGHTS))
 
+    alignment_model, alignment_tokenizer = load_alignment_model(
+      Device.Cpu,
+      dtype=TTYPES[Device.Cpu]
+    )
+    whisper_model = faster_whisper.WhisperModel(
+      Model.Large,
+      device=Device.Cpu,
+      compute_type=MTYPES[Device.Cpu]
+    )
+    whisper_pipeline = faster_whisper.BatchedInferencePipeline(whisper_model)
+
     for i in list_mp3(options.input_folder):
-        print(i)
+        stime = time.time()
+        print(transcribe(
+          i,
+          whisper_pipeline,
+          whisper_model,
+          alignment_model,
+          alignment_tokenizer,
+          [-1],
+          options.whisper_batch,
+          options.torch_batch,
+          LANGUAGE
+        ), int(time.time() - stime), 'sec')
 
 
 if __name__ == '__main__':  # pragma: no cover
