acefe810ecd677fddafd2352219975a240c4d9d2
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-05-02 10:48:45 +0400

    refactor

--- voice_transcription/audio.py
+++ voice_transcription/audio.py
@@ -124,17 +124,50 @@ def get_cut_positions(silence_chunks, chunk_length_ms):
     return cuts
 
 
-def split_audio(input_file, out_file_mask, chunk_length_ms, output_format):
-    """Split input audio file by chunks with given size."""
-    audio = AudioSegment.from_file(input_file)
-    silence_chunks = detect_silence(audio, min_silence_len=MIN_SILENCE_LEN, silence_thresh=SILENCE_THRESHOLD)
+def split_on_silence_min_length(
+    audio,
+    min_silence_len=1000, silence_thresh=-16, seek_step=1,
+    min_chunk_length=0
+):
+    """Return list of audio segments from splitting audio_segment on silent sections.
+
+    audio_segment - original pydub.AudioSegment() object
+
+    min_silence_len - (in ms) minimum length of a silence to be used for
+        a split. default: 1000ms
+
+    silence_thresh - (in dBFS) anything quieter than this will be
+        considered silence. default: -16dBFS
+
+    seek_step - step size for interating over the segment in ms
+
+    min_chunk_length - minimal chunk length (in ms).
+        default: 0 (no minimal chunk length, split by middle of silence segments)
+    """
+    silence_chunks = detect_silence(
+        audio,
+        min_silence_len=min_silence_len,
+        silence_thresh=silence_thresh,
+        seek_step=seek_step
+    )
     chunks = []
     start = 0
-    for i in get_cut_positions(silence_chunks, chunk_length_ms):
+    for i in get_cut_positions(silence_chunks, min_chunk_length):
         chunks.append(audio[start:i])
         start = i
     chunks.append(audio[start:])
 
+    return chunks
+
+
+def split_audio(input_file, out_file_mask, chunk_length_ms, output_format):
+    """Split input audio file by chunks with given size."""
+    chunks = split_on_silence_min_length(
+      AudioSegment.from_file(input_file),
+      min_silence_len=MIN_SILENCE_LEN,
+      silence_thresh=SILENCE_THRESHOLD,
+      min_chunk_length=chunk_length_ms
+    )
     # Save chunks in parallel using ThreadPoolExecutor
     with ThreadPoolExecutor() as executor:
         for i, chunk in enumerate(chunks):
