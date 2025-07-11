3d48b1476f8d55a9968924c72a86f47c8fe1c1b5
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-07-11 15:03:27 +0400

    step

--- cli/to_json.py
+++ cli/to_json.py
@@ -1,15 +1,17 @@
 """Transcibe mp3 to json with word data."""
+import os
 import time
 import argparse
 import sys
 from datetime import datetime
 import json
+import shutil
 
 import faster_whisper
 
 sys.path.insert(1, '.')
-from voice_transcription import Model, Device, MTYPES, progress_bar
-from voice_transcription.whisper import segments_to_json, msec
+from voice_transcription import Model, Device, MTYPES, progress_bar, join_jsons
+from voice_transcription.whisper import make_json, segments_to_json, msec
 
 VERSION = '1.0'
 COPYRIGHTS = 'Copyrights by Vitaly Bogomolov 2025'
@@ -23,38 +25,44 @@ PARSER.add_argument(
   "out_file",
   help="Json file for output."
 )
+PARSER.add_argument(
+  "--temp_folder",
+  default='temp',
+  help="Folder for temp files.",
+)
+PARSER.add_argument(
+  "--max_length",
+  type=int,
+  default=90 * 60,
+  help="Split the input file into parts if duration exceeds the parameter value. Default is 5400 (90 minutes).",
+)
 
 
-def main(options):  # pylint: disable=too-many-locals
+def main(options):
     """Entry point."""
     print("Whisper to json transcribe tool v.{}. {}".format(VERSION, COPYRIGHTS))
     stime = time.time()
+    tasks = get_tasks(options.mp3_file, options.temp_folder, options.max_length)
 
-    print("Loading model...")
+    print("Loading Whisper model...")
     whisper_model = faster_whisper.WhisperModel(
       Model.Large,
       device=Device.Cpu,
       compute_type=MTYPES[Device.Cpu]
     )
-    print("Decode wave...")
-    waveform = faster_whisper.decode_audio(options.mp3_file)
 
-    print("Creating segments...")
-    segments, info = whisper_model.transcribe(
-      waveform, 'ru', suppress_tokens=[-1],
-      vad_filter=True,
-      word_timestamps=True
-    )
-    duration = msec(info.duration_after_vad)
-    print("Duration", duration, "msec")
+    os.makedirs(options.temp_folder, exist_ok=True)
+    data = join_jsons([
+      make_json(whisper_model, i, progress_bar)
+      for i in tasks
+    ])
 
-    now = datetime.utcnow()
-    data = segments_to_json(segments, duration, progress_bar, now)
-    progress_bar(duration, duration, now)
     print("\nSaving json {}...".format(options.out_file))
 
     with open(options.out_file, "wt", encoding="utf8") as out:
         out.write(json.dumps(data, indent=4, ensure_ascii=False).encode('utf8').decode())
+
+    shutil.rmtree(options.temp_folder)
 
     print(options.out_file, "{} sec".format(int(time.time() - stime)))
     return 0
--- voice_transcription/whisper.py
+++ voice_transcription/whisper.py
@@ -1,4 +1,6 @@
 """Whisper segments processing."""
+from datetime import datetime
+import faster_whisper
 
 
 def msec(sec):
@@ -25,5 +27,26 @@ def segments_to_json(segments, total_msec, progress_bar, utc_time_start):
             words.append([msec(word.start), msec(word.end - word.start), word.word.strip()])
         seg_data.append(words)
         data.append(seg_data)
+
+    return data
+
+
+def make_json(whisper_model, mp3_file, progress_bar):
+    """Return json data for given mp3 file name."""
+    print("Decode {} to wave ...".format(mp3_file))
+    waveform = faster_whisper.decode_audio(mp3_file)
+
+    print("Creating segments...")
+    segments, info = whisper_model.transcribe(
+      waveform, 'ru', suppress_tokens=[-1],
+      vad_filter=True,
+      word_timestamps=True
+    )
+    duration = msec(info.duration_after_vad)
+    print("Duration", duration, "msec")
+
+    now = datetime.utcnow()
+    data = segments_to_json(segments, duration, progress_bar, now)
+    progress_bar(duration, duration, now)
 
     return data
